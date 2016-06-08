54c9c328c7d0c7236151e38cb6c9bb8eebefee43
yesicarojas <yesica10_09@hotmail.com>
2016-06-08 22:01:39 +0200

    cookies y tag video

---
+++ WEBFINAL/js/cookies.js
@@ -0,0 +1,61 @@
+/*
+<!--
+Autor= Yesica Rojas, Ruben Rodriguez, Nassim Bousaidi
+Fecha= 10.junio de 2016
+Licencia=  gp130
+Version= 1.0
+Descripcion= Página contacto que contiene el formulario
+-->
+<!--
+  Copyright (C) 2016 Yesica Rojas, Ruben Rodriguez, Nassim Bousaidi
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function getCookie(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1){
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1){
+        c_value = null;
+    }else{
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1){
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+ 
+function setCookie(c_name,value,exdays){
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
+ 
+if(getCookie('HandsMade')!="1"){
+    document.getElementById("barraaceptacion").style.display="block";
+}
+function PonerCookie(){
+    setCookie('HandsMade','1',365);
+    document.getElementById("barraaceptacion").style.display="none";
+}
+
+<!--//FIN BLOQUE COOKIES-->
+
